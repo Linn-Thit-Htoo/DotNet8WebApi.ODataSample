38721cd36b768112cd67dd41d6ad4f6c25bcb72d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 21:30:45 +0630

    -

--- DotNet8WebApi.ODataSample/Resource/MessageResource.cs
+++ DotNet8WebApi.ODataSample/Resource/MessageResource.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8WebApi.ODataSample.Resource
+﻿namespace DotNet8WebApi.ODataSample.Resource;
+
+public static class MessageResource
 {
-    public static class MessageResource
-    {
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string NotFound { get; } = "No Data Found.";
-        public static string DeleteSucces { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-    }
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string NotFound { get; } = "No Data Found.";
+    public static string DeleteSucces { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
 }
