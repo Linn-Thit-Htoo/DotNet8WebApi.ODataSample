2efa22d53650b611dfc707f43b593457ac16effb
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 21:31:34 +0630

    -

---
+++ DotNet8WebApi.ODataSample/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using DotNet8WebApi.ODataSample.Database;
+global using DotNet8WebApi.ODataSample.Models;
+global using Microsoft.AspNetCore.OData;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.OData.Edm;
+global using Microsoft.OData.ModelBuilder;
